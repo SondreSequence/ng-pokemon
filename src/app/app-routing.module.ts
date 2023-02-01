c824b617a6f5da6e437ce2eb73d0340cf62f1b2e
Johann Braaten <johann.braaten@no.experis.com>
2023-02-01 11:42:40 +0100

    feat: added auth-guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 import { LoginPage } from './pages/login/login.page';
 import { PokemonCataloguePage } from './pages/pokemon-catalogue/pokemon-catalogue.page';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/login' },
   { path: 'login', component: LoginPage },
-  { path: 'catalogue', component: PokemonCataloguePage },
+  {
+    path: 'catalogue',
+    component: PokemonCataloguePage,
+    canActivate: [AuthGuard],
+  },
 ];
 
 @NgModule({
