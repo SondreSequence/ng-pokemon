e5c7a9b1982651cf2b2ff4be4d31ab0225af4c44
Larsjoar96 <32615350+Larsjoar96@users.noreply.github.com>
2023-02-03 09:53:27 +0100

    Preparing for merge with Sondre

--- src/app/components/pokemon-api/pokemon-api.component.ts
+++ src/app/components/pokemon-api/pokemon-api.component.ts
@@ -10,39 +10,58 @@ import { CaughtPokemonService } from 'src/app/services/caught-pokemon.service';
   styleUrls: ['./pokemon-api.component.css']
 })
 
-
-
 export class PokemonApiComponent implements OnInit {
-
-  constructor(private apiService: ApiService, private caughtPokemonService: CaughtPokemonService) {}
-
+  constructor(private apiService: ApiService) {}
   public pokemonArray: { name: string, image: string }[] = [];
 
   public onButtonClick(pokemon: Pokemon, cardElement: HTMLElement, pokeball: HTMLElement ) {
     cardElement.className = 'card animate__animated animate__flip';
-    setInterval(() => {
+    setTimeout(() => {
       cardElement.className = 'caught';
       pokeball.style.display = 'block';
     }, 800);
     console.log(pokemon.name)
 
-    this.caughtPokemonService.addToCaughtPokemon(pokemon)
+    //this.caughtPokemonService.addToCaughtPokemon(pokemon)
+  }
+
+  public rickAstely(cardContainer: HTMLDivElement, pokeball: HTMLElement, input: HTMLElement){
+    let rick = {name: "Rick Astely", image: "assets/rick.png"}
+
+    this.pokemonArray.unshift(rick);
+    pokeball.className = "pokeball animate__animated animate__rollOut"
+    input.className = '';
+    
+    let firstLi : HTMLLIElement | null = null; //First element in the list
+    setTimeout(() => {
+      input.className = "form-text anim-typewriter";
+      input.innerHTML = "Never gonna give you up!";
+      
+      firstLi = cardContainer.querySelector('li');
+      if (firstLi) {
+        const firstImage = firstLi.querySelector("img");
+        if (firstImage && firstImage.style) {
+          
+          }
+        firstLi.className = "card animate__animated animate__bounceInDown";
+      }
+    }, 3);
+    setTimeout(() => { firstLi = cardContainer.querySelector('li'); if (firstLi) firstLi.className = "card animate__animated animate__swing animate__infinite";},1000)
   }
 
  public inputChange(pokemon: Pokemon, input: HTMLElement ) {
     input.className = '';
-    setInterval(() => {
+    setTimeout(() => {
       input.className = "form-text anim-typewriter";
-    }, 2);
+    }, 1);
     input.innerHTML = "You caught " + pokemon.name + "!";
   } 
 
   public handleMouseEnter(pokemonImg: HTMLElement){
-    pokemonImg.className = 'animate__animated animate__bounce';
-  }
-
-  public handleMouseLeave(pokemonImg: HTMLElement){
-    pokemonImg.className = '';
+      pokemonImg.className = 'animate__animated animate__bounce';
+    setTimeout(() => {
+      pokemonImg.className = '';
+    }, 1000);
   }
 
 
